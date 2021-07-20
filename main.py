58cefd204ca9361079d826b0840e06280e96f3b6
Utkarsh <utkarsh995@gmail.com>
2021-07-20 13:30:10 +0000

    commit 1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from utils.logging import logger
 from alexa import Alexa
 from tuya import Switch
-import datetime
+import datetime, time
 
 INTERVAL_MINUTES=2
 
@@ -16,13 +16,25 @@ class Runner:
         self._switch=Switch()
     
     def run(self):
-        switch_status=self._switch.is_on()
-        if switch_status:
-            self._alexa.announce(f'Pump is running for greater than {self._runtime} minutes')
-            self._runtime+=INTERVAL_MINUTES
-        else:
-            self._runtime=0
-            if self._last_status:
-                self._alexa.announce(f'Pump is switched off. Closed.')
-        self._last_status=switch_status
-        self._last_run=datetime.now()
+        while True:
+            logger.info("Checking....")
+            switch_status=self._switch.is_on()
+            if switch_status:
+                self._alexa.announce(f'Pump is running for greater than {self._runtime} minutes')
+                self._runtime+=INTERVAL_MINUTES
+            else:
+                self._runtime=0
+                if self._last_status:
+                    self._alexa.announce(f'Pump is switched off. Closed.')
+            self._last_status=switch_status
+            self._last_run=datetime.datetime.now()
+            time.sleep(INTERVAL_MINUTES*60)
+            logger.info("Check Completed")
+
+while True:
+    try:
+        runner=Runner()
+        runner.run()
+    except Exception as e:
+        logger.exception('Something went wrong...Sleeping for 2 minutes')
+        time.sleep(120)
